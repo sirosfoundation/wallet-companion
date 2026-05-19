16cf1b02e6a4ab81a77b872583ee14eebd3a6741
Simon <contact@smn.codes>
2026-05-19 10:54:25 +0200

    fix: add missing protocols file

---
+++ src/shared/protocols.ts
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview
+ * Protocols supported by the extension and registered wallets.
+ */
+
+/**
+ * OpenID4VP protocol variants supported by the extension and wallets.
+ * Wallets can support one or more of these variants, and the extension will
+ * indicate which ones are supported when showing the wallet selector.
+ */
+export enum OpenID4VPProtocols {
+	NORMAL = 'openid4vp',
+	UNSIGNED = 'openid4vp-v1-unsigned',
+	SIGNED = 'openid4vp-v1-signed',
+	MULTISIGNED = 'openid4vp-v1-multisigned',
+}
