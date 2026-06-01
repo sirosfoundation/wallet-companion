27fa5deea72a99ba5267a1935073212d480a144c
Simon <contact@smn.codes>
2026-06-01 18:48:35 +0200

    chore: add todo

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,3 +1,5 @@
 import { Logger } from '@sirosfoundation/browser-log';
 
+
+// TODO: This should be configurable to different build targets (e.g. 'debug' for development, 'info' for production)
 export const logger = new Logger({ level: 'debug' });
