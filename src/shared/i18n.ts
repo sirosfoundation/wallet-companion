2b22303e27e9ca55a15ea4caa6f13eaadaaa8baa
Simon <contact@smn.codes>
2026-06-22 17:26:10 +0200

    feat: fall back to imported locals, useful for injected scripts that don't have access to the browser api

--- src/shared/i18n.ts
+++ src/shared/i18n.ts
@@ -1,9 +1,58 @@
 import { browserApi } from './browser-api';
+import elMessages from '../../_locales/el/messages.json';
+import enMessages from '../../_locales/en/messages.json';
+import fiMessages from '../../_locales/fi/messages.json';
+import ptMessages from '../../_locales/pt_PT/messages.json';
+import svMessages from '../../_locales/sv/messages.json';
+
+type Locale = {
+	label: string;
+	messages: Record<
+		string,
+		{
+			message: string;
+			description?: string;
+		}
+	>;
+};
+
+type Locales = Record<string, Locale>;
+
+const locales = {
+	el: {
+		label: 'Ελληνικά',
+		messages: elMessages,
+	},
+	en: {
+		label: 'English',
+		messages: enMessages,
+	},
+	fi: {
+		label: 'Suomi',
+		messages: fiMessages,
+	},
+	pt_PT: {
+		label: 'Português (Portugal)',
+		messages: ptMessages,
+	},
+	sv: {
+		label: 'Svenska',
+		messages: svMessages,
+	},
+ } satisfies Locales ;
 
 export function getMessage(key: string, substitutions?: string | string[]): string {
 	if (browserApi?.i18n?.getMessage) {
 		const msg = browserApi.i18n.getMessage(key, substitutions);
 		return msg || key;
+	}
+
+	if (typeof navigator !== 'undefined') {
+		const lang = navigator.language.split('-')[0] as keyof typeof locales;
+
+		const locale = locales[lang] ?? locales.en;
+
+		return locale.messages[key as keyof typeof locale.messages]?.message || key;
 	}
 
 	return key;
