7ce8a954d0fced284233323c1ff983d71aeeeda2
Simon <contact@smn.codes>
2026-06-23 11:41:28 +0200

    fix: make sure that runtime is availabe for before returning `browserApi`, we want this to be null in contexts where this is not available

--- src/shared/browser-api.ts
+++ src/shared/browser-api.ts
@@ -2,8 +2,14 @@
  * Browser API abstraction to support both multiple browsers.
  * Provides a unified interface for accessing the browser API, whether it's the WebExtension API (for Firefox) or the Chrome Extension API.
  */
-export const browserApi = typeof browser !== 'undefined'
-	? browser
-	: typeof chrome !== 'undefined'
-		? chrome
-		: null;
+export const browserApi = (() => {
+	if (typeof browser !== 'undefined' && 'runtime' in browser) {
+		return browser;
+	}
+
+	if (typeof chrome !== 'undefined' && 'runtime' in chrome) {
+		return chrome;
+	}
+
+	return null;
+})();
